5a9419cdccc6d04d326c4575fdf8a847e8367587
Gustavo A. Gutkoski <gustavogutkoski2609@gmail.com>
2022-07-29 11:18:04 -0300

    feat: create type deck and methods to create a deck and print the cards

---
+++ playing-cards/deck.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cards = addCardsToDeck()
+
+	return cards
+}
+
+func addCardsToDeck() deck {
+	cardSuits := []string{"Ace", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"One", "Two", "Three", "Four"}
+
+	deck := deck{}
+	for _, cardSuit := range cardSuits {
+		for _, cardValue := range cardValues {
+			deck = append(deck, cardSuit+" of "+cardValue)
+		}
+	}
+
+	return deck
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(card, i)
+	}
+}
